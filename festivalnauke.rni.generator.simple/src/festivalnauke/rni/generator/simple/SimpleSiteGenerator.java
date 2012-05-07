07bd0e71e2249fc197fd1f51eb825c41a6b201ee
Igor Dejanovic <igor.dejanovic@gmail.com>
2012-05-07 20:27:25 +0200

    Initial version of simple site generator.

---
+++ festivalnauke.rni.generator.simple/src/festivalnauke/rni/generator/simple/SimpleSiteGenerator.java
@@ -0,0 +1,11 @@
+package festivalnauke.rni.generator.simple;
+
+import festivalnauke.rni.napravisvojsajt.generator.ISiteGenerator;
+
+public class SimpleSiteGenerator implements ISiteGenerator {
+
+	public SimpleSiteGenerator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
